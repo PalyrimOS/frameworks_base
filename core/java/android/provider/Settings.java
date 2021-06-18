4261e0a77f8a94ebc97abb014da2c7baf7f755d5
jhenrique09 <jhsv09@gmail.com>
2021-06-18 11:54:32 -0300

    [1/2] Allow changing face unlock method when locked
    Change-Id: I9a8e812bbe869dead94d631fa0a2f53f3bf67cc6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10902,6 +10902,12 @@ public final class Settings {
                 "device_state_rotation_lock";
 
         /**
+         * Face Unlock Method
+         * @hide
+         */
+        public static final String FACE_UNLOCK_METHOD = "face_unlock_method";
+
+        /**
          * Control whether communal mode is allowed on this device.
          *
          * @hide
