79c0f427dc48238b3108a68ef19d9b552d0ffb87
Genkzsz11 <genkzsz11@gmail.com>
2022-08-28 15:57:28 +0700

    SystemUI: Update gamespace for A13

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -4471,6 +4471,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
         return mGutsManager;
     }
 
+    @Override
     public GameSpaceManager getGameSpaceManager() {
         return mGameSpaceManager;
     }
